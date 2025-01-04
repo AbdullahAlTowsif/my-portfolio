25e56c545ff903146ba9cb8381a5500dcd476874
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-01-05 01:25:42 +0600

    proptypes disabled

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 
+// eslint-disable-next-line react/prop-types
 const Projects = ({project}) => {
+    // eslint-disable-next-line react/prop-types
     const {id, name, image} = project;
     const navigate = useNavigate();
     const handleDetails = () => {
